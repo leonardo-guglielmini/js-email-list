244f33634eb63bc673303b6b45e048e23e5365e6
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-05 15:49:38 +0100

    ADD html | email-display section ADD js | getEmailList function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 console.log("Email List");
 
+const emailDisplay = document.getElementById("email-display");
+
 function getEmailList() {
     let emailList = [];
+    const ulEl = document.createElement("ul");
+
     for (let i = 0; i < 10; i++) {
         axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res) => {
                 //console.log("Success", res, res.data);
                 emailList.push(res.data.response);
+                const liEl = document.createElement("li");
+                liEl.innerHTML = res.data.response;
+                ulEl.appendChild(liEl);
             })
             .catch((err) => {
                 alert("Error", err);
             })
     }
-    console.log(emailList);
+    emailDisplay.appendChild(ulEl);
 }
 
 getEmailList();
